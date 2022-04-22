1e5dc7507a58b003da281f754a82ee0de1cfcb4f
Hà Huy Bút <hahuybut@gmail.com>
2022-04-22 10:28:13 +0700

    task4

---
+++ Task4/B19_DisplayPattern.java
@@ -0,0 +1,11 @@
+public class B19_DisplayPattern {
+    public static void main(String[] args) {
+        int row, col;
+        for(row = 1; row<=5; row++){
+            for(col=1; col<=row; col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
