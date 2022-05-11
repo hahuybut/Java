1582efa4b0ce16cb97d11677f59f29b2d2c8fcb3
Hà Huy Bút <93537682+hahuybut@users.noreply.github.com>
2022-05-11 15:44:54 +0700

    Delete TestAccount.java

--- Exam/TestAccount.java
+++
@@ -1,8 +0,0 @@
-public class TestAccount {
-    public static void main(String[] args){
-        Account acc = new Account();
-        acc.input();
-        acc.DepositAndWithdraw();
-        System.out.println(acc);
-    }
-}
